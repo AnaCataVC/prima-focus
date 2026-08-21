7844fd33ec58e14c27bdf4b8549d7d2b532a4e06
AnaCataVC <anacatalina@outlook.cl>
2026-08-21 19:58:04 -0400

    chore: bump version to v1.5.0

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ancata.prima_focus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.4.0"
+        versionCode = 5
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
