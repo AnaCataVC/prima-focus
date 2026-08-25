f8fce4bedfaf46310b56266f5b51c8352e1ee14f
AnaCataVC <anacatalina@outlook.cl>
2026-08-25 08:48:19 -0400

    chore(release): bump version to 1.7.0 (versionCode 7)

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ancata.prima_focus"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.6.0"
+        versionCode = 7
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
